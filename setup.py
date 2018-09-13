bfd0bdd780699a880e03cbe09ebe2b34d0ad3c89
Nicholas Christian <nd.christian@gmail.com>
2018-09-13 13:03:35 -0700

    Bump to 0.3dev and refactor entry point

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
+import setuptools
 from distutils.core import setup
 
 setup(
     name='LowEarthOrbit',
-    version='0.4dev',
+    version='0.3dev',
     author='Nicholas Christian',
     author_email='ndchristian@gmail.com',
-    packages=['lowearthorbit'],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
     license='LICENSE.txt',
     description='A better way to deploy AWS CloudFormation',
     long_description=open('README.txt').read(),
@@ -13,8 +15,9 @@ setup(
                       'botocore',
                       'boto3'
                       ],
-    entry_points='''
-    [console_scripts]
-    leo=leo:cli
-    '''
+    entry_points={
+        'console_scripts': [
+            'leo = lowearthorbit.leo:cli'
+        ]
+    },
 )
