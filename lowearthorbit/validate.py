0692804d4301ad4dbbcd791d31bb4591c1d59038
Nicholas Christian <nd.christian@gmail.com>
2018-10-22 19:47:16 -0700

    Handle nonexistent prefixes while validating

--- lowearthorbit/validate.py
+++ lowearthorbit/validate.py
@@ -26,18 +26,21 @@ def validate_templates(**kwargs):
     cfn_ext = ('.json', '.template', '.txt', 'yaml', 'yml')
 
     validation_errors = []
-    for object in s3_client.list_objects_v2(**validate_parameters
-                                            )['Contents']:
-        if object['Key'].endswith(cfn_ext):
-            template_url = s3_client.generate_presigned_url('get_object',
-                                                            Params={'Bucket': kwargs['bucket'],
-                                                                    'Key': object['Key']},
-                                                            ExpiresIn=60)
-            try:
-                cf_client.validate_template(TemplateURL=template_url)
-                click.echo("Validated %s" % object['Key'])
+    try:
+        for object in s3_client.list_objects_v2(**validate_parameters
+                                                )['Contents']:
+            if object['Key'].endswith(cfn_ext):
+                template_url = s3_client.generate_presigned_url('get_object',
+                                                                Params={'Bucket': kwargs['bucket'],
+                                                                        'Key': object['Key']},
+                                                                ExpiresIn=60)
+                try:
+                    cf_client.validate_template(TemplateURL=template_url)
+                    click.echo("Validated %s" % object['Key'])
 
-            except botocore.exceptions.ClientError as e:
-                validation_errors.append({'Template': object['Key'], 'Error': e})
+                except botocore.exceptions.ClientError as e:
+                    validation_errors.append({'Template': object['Key'], 'Error': e})
+    except KeyError:
+        log.error("The specified key does not exist in %s" % kwargs['bucket'])
 
     return validation_errors
