24a82fb7de7a2a84a5ee5fe8fb40d8877e1f787a
Nicholas Christian <nd.christian@gmail.com>
2018-10-31 14:38:13 -0700

    Rework deploy so everything can be gated

--- lowearthorbit/resources/create.py
+++ lowearthorbit/resources/create.py
@@ -7,6 +7,7 @@ import botocore
 import click
 
 from lowearthorbit.resources.capabilities import get as get_capabilities
+from lowearthorbit.resources.changes import display_changes, change_set_delete_waiter
 from lowearthorbit.resources.parameters import gather as gather_parameters
 
 log = logging.getLogger(__name__)
@@ -83,15 +84,39 @@ def transform_template(cfn_client, stack_name, template_url, stack_parameters, d
     cfn_client.get_waiter('change_set_create_complete').wait(
         ChangeSetName=transform_stack['Id']
     )
-    cfn_client.execute_change_set(
-        ChangeSetName=transform_stack['Id'],
-        StackName=stack_name
-    )
-    log.debug('Executing change set')
 
-    return cfn_client.describe_stacks(StackName=stack_name)['Stacks'][0]
+    return transform_stack
 
 
+def display_status(cfn_client, current_stack, stack_name):
+    STACK_LIST.append({'StackId': current_stack['StackId'], 'StackName': stack_name})
+    stack_description = cfn_client.describe_stacks(StackName=current_stack['StackId'])['Stacks'][0][
+        'Description']
+    click.echo("\nCreating {}...".format(stack_name))
+    click.echo("Description of {}: \n\t{}".format(stack_name, stack_description))
+    try:
+        cfn_client.get_waiter('stack_create_complete').wait(StackName=current_stack['StackId'])
+        click.echo("Created {}.".format(stack_name))
+
+        return {'StackName': stack_name}
+    except botocore.exceptions.WaiterError:
+        click.echo("\n{} is currently rolling back.".format(stack_name))
+        resource_failures = [{'LogicalResourceId': event['LogicalResourceId'],
+                              'ResourceStatusReason': event['ResourceStatusReason']} for event in
+                             cfn_client.describe_stack_events(StackName=current_stack['StackId'])['StackEvents']
+                             if event['ResourceStatus'] == 'CREATE_FAILED']
+
+        if resource_failures:
+            for failures in resource_failures:
+                click.echo("%s has failed to be created because: '%s'" % (
+                    failures['LogicalResourceId'], failures['ResourceStatusReason']))
+        else:
+            click.echo("Please check console for why some resources failed to create.")
+
+        sys.exit()
+
+
+# noinspection PyUnboundLocalVariable
 def create_stack(**kwargs):
     """Creates the stack and handles rollback conditions"""
 
@@ -99,6 +124,7 @@ def create_stack(**kwargs):
     key_object = kwargs['key_object']
     bucket = kwargs['bucket']
     job_identifier = kwargs['job_identifier']
+    gated = kwargs['gated']
     parameters = kwargs['parameters']
     deploy_parameters = kwargs['deploy_parameters']
 
@@ -119,46 +145,65 @@ def create_stack(**kwargs):
                                          job_identifier=job_identifier)
 
     try:
-        if 'DeclaredTransforms' not in template_summary:
-            log.debug('Creating stack')
-            current_stack = cfn_client.create_stack(StackName=stack_name,
-                                                    TemplateURL=template_url,
-                                                    Parameters=stack_parameters,
-                                                    Capabilities=stack_capabilities,
-                                                    DisableRollback=False,
-                                                    TimeoutInMinutes=123,
-                                                    **deploy_parameters)
+        transformed = False
+        if 'DeclaredTransforms' in template_summary:
+            transformed_stack = transform_template(cfn_client=cfn_client,
+                                                   stack_name=stack_name,
+                                                   template_url=template_url,
+                                                   stack_parameters=stack_parameters,
+                                                   deploy_parameters=deploy_parameters)
+
+            # Checks for the changes
+            change_set_details = cfn_client.describe_change_set(ChangeSetName=transformed_stack['Id'])
+            change_set_changes = change_set_details['Changes']
+            display_changes(changes=change_set_changes, name=stack_name, change_set=True)
+            transformed = True
+
         else:
-            current_stack = transform_template(cfn_client=cfn_client,
-                                               stack_name=stack_name,
-                                               template_url=template_url,
-                                               stack_parameters=stack_parameters,
-                                               deploy_parameters=deploy_parameters)
+            display_changes(changes=template_summary['ResourceTypes'], name=stack_name, change_set=False)
 
-        STACK_LIST.append({'StackId': current_stack['StackId'], 'StackName': stack_name})
-        stack_description = cfn_client.describe_stacks(StackName=current_stack['StackId'])['Stacks'][0]['Description']
-        click.echo("\nCreating {}...".format(stack_name))
-        click.echo("Description of {}: \n\t{}".format(stack_name, stack_description))
-        try:
-            cfn_client.get_waiter('stack_create_complete').wait(StackName=current_stack['StackId'])
-            click.echo("Created {}.".format(stack_name))
+        if gated:
+            execute_changes = click.confirm("Would you like to deploy?")
+            if execute_changes:
+                if transformed:
+                    # noinspection PyUnboundLocalVariable
+                    cfn_client.execute_change_set(ChangeSetName=transformed_stack['Id'],
+                                                  StackName=stack_name)
+                    current_stack = cfn_client.describe_stacks(StackName=stack_name)['Stacks'][0]
+                else:
+                    current_stack = cfn_client.create_stack(StackName=stack_name,
+                                                            TemplateURL=template_url,
+                                                            Parameters=stack_parameters,
+                                                            Capabilities=stack_capabilities,
+                                                            DisableRollback=False,
+                                                            TimeoutInMinutes=123,
+                                                            **deploy_parameters)
 
-            return {'StackName': stack_name}
-        except botocore.exceptions.WaiterError:
-            click.echo("\n{} is currently rolling back.".format(stack_name))
-            resource_failures = [{'LogicalResourceId': event['LogicalResourceId'],
-                                  'ResourceStatusReason': event['ResourceStatusReason']} for event in
-                                 cfn_client.describe_stack_events(StackName=current_stack['StackId'])['StackEvents']
-                                 if event['ResourceStatus'] == 'CREATE_FAILED']
+                display_status(cfn_client=cfn_client, current_stack=current_stack, stack_name=stack_name)
 
-            if resource_failures:
-                for failures in resource_failures:
-                    click.echo("%s has failed to be created because: '%s'" % (
-                        failures['LogicalResourceId'], failures['ResourceStatusReason']))
             else:
-                click.echo("Please check console for why some resources failed to create.")
+                if transformed:
+                    click.echo("Deleting change set {}...".format(transformed_stack['Id']))
+                    cfn_client.delete_change_set(ChangeSetName=transformed_stack['Id'],
+                                                 StackName=stack_name)
+                    # Check if still needed
+                    change_set_delete_waiter(change_set_id=transformed_stack['Id'], cfn_client=cfn_client)
 
-            sys.exit()
+        else:
+            if transformed:
+                cfn_client.execute_change_set(ChangeSetName=transformed_stack['Id'],
+                                              StackName=stack_name)
+                current_stack = cfn_client.describe_stacks(StackName=stack_name)['Stacks'][0]
+            else:
+                current_stack = cfn_client.create_stack(StackName=stack_name,
+                                                        TemplateURL=template_url,
+                                                        Parameters=stack_parameters,
+                                                        Capabilities=stack_capabilities,
+                                                        DisableRollback=False,
+                                                        TimeoutInMinutes=123,
+                                                        **deploy_parameters)
+
+            display_status(cfn_client=cfn_client, current_stack=current_stack, stack_name=stack_name)
 
     except botocore.exceptions.ClientError as e:
         raise e
--- lowearthorbit/resources/update.py
+++ lowearthorbit/resources/update.py
@@ -1,126 +1,14 @@
 import logging
-import sys
 import time
 
 import botocore
 import click
 
 from lowearthorbit.resources.capabilities import get as get_capabilities
+from lowearthorbit.resources.changes import display_changes, apply_changes, change_set_delete_waiter
 from lowearthorbit.resources.parameters import gather as gather_parameters
 
 log = logging.getLogger(__name__)
-
-
-def stack_continue_rollback_waiter(stack_name, cfn_client):
-    """Boto3 does not have a waiter for creating rolling back a stack completely, this is a custom one"""
-
-    log.debug('Stack is rolling back')
-
-    stack_continue_rollback_counter = 0
-    stack_continue_rollback = True
-
-    while stack_continue_rollback:
-        stack_details = cfn_client.describe_stacks(StackName=stack_name)['Stacks'][0]
-        if stack_details['StackStatus'] in 'UPDATE_ROLLBACK_COMPLETE':
-            stack_continue_rollback = False
-
-        elif stack_details['StackStatus'] in 'UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS':
-            if stack_continue_rollback_counter == 120:
-                click.echo("\n{}".format(stack_details['StatusReason']))
-                sys.exit("Something went wrong while trying to rollback the stack. Please check the console.")
-            else:
-                stack_continue_rollback_counter += 1
-                time.sleep(15)
-
-        elif stack_details['StackStatus'] in 'UPDATE_ROLLBACK_FAILED':
-            sys.exit(
-                "Rolling back for {} has failed. Please check the console.".format(stack_details['StackName']))
-
-
-def change_set_delete_waiter(change_set_id, cfn_client):
-    """Boto3 does not have a waiter for deleting a change set, this is a custom one"""
-
-    log.debug('Deleting change set')
-
-    try:
-        change_set_deletion_counter = 0
-        change_set_deletion_bool = True
-
-        # Custom change set deletion waiter
-        while change_set_deletion_bool:
-            change_set_details = cfn_client.describe_change_set(ChangeSetName=change_set_id)
-            if change_set_details['Status'] in 'DELETE_COMPLETE':
-                pass
-                change_set_deletion_bool = False
-
-            elif change_set_details['Status'] in 'FAILED':
-                click.echo("\n{}".format(change_set_details['StatusReason']))
-                sys.exit("Change set {} deletion has failed. Please check the console.".format(
-                    change_set_details['StackName']))
-
-            else:
-                if change_set_deletion_counter == 120:
-                    click.echo("\n{}".format(change_set_details['StatusReason']))
-                    sys.exit(
-                        "Something went wrong while deleting the change set. Please check the console.")
-                else:
-                    change_set_deletion_counter += 1
-                    time.sleep(15)
-
-    except botocore.exceptions.ClientError:
-        # Change set is already deleted therefore the waiter is no longer needed.
-        pass
-
-
-def change_log(changes):
-    """Prints out all the possible changes to the CloudFormation stack"""
-
-    data = []
-    for change in changes:
-        try:
-            replacement = change['ResourceChange']['Replacement']
-        except KeyError:
-            replacement = "None"
-
-        try:
-            phyresid = change['ResourceChange']['PhysicalResourceId']
-        except KeyError:
-            phyresid = "None"
-
-        data.append(["Action: {}".format(change['ResourceChange']['Action']),
-                     "Logical ID: {}".format(change['ResourceChange']['LogicalResourceId']),
-                     "Physical ID: {}".format(phyresid),
-                     "Resource Type: {}".format(change['ResourceChange']['ResourceType']),
-                     "Replacement: {}".format(replacement)])
-    return data
-
-
-def apply_changes(cfn_client, update_stack_name, past_failures, change_set_name, change_set):
-    click.echo("Executing change set: {}...".format(change_set_name))
-    cfn_client.execute_change_set(ChangeSetName=change_set['Id'], StackName=update_stack_name)
-
-    try:
-        cfn_client.get_waiter('stack_update_complete').wait(StackName=update_stack_name)
-    except botocore.exceptions.WaiterError:
-        resource_failures = []
-        for event in cfn_client.describe_stack_events(StackName=update_stack_name)['StackEvents']:
-            if event['ResourceStatus'] in ['CREATE_FAILED', 'UPDATE_FAILED']:
-                resource_failures.append(event)
-        if resource_failures:
-            for failures in resource_failures:
-                if failures not in past_failures:
-                    click.echo("%s has failed to be updated because: '%s'" % (
-                        failures['LogicalResourceId'], failures['ResourceStatusReason']))
-        else:
-            click.echo("Please check console for why some resources failed to update.")
-
-        stack_status = cfn_client.describe_stacks(StackName=update_stack_name)['Stacks'][0]['StackStatus']
-        if stack_status == 'UPDATE_ROLLBACK_FAILED':
-            click.echo("Attempting to restore former state...")
-            cfn_client.continue_update_rollback(StackName=update_stack_name)
-            stack_continue_rollback_waiter(stack_name=update_stack_name, cfn_client=cfn_client)
-
-        sys.exit("Stopped update because of %s's updating failure." % update_stack_name)
 
 
 def update_stack(**kwargs):
@@ -182,23 +70,7 @@ def update_stack(**kwargs):
     change_set_changes = change_set_details['Changes']
 
     if change_set_changes:
-        click.echo("Gathering changes for change set: {}...".format(change_set_name))
-        for change in change_log(changes=change_set_changes):
-            # Dynamically gathers the length of each change in order to display information better
-            action = sorted([len(item[0]) for item in change])[-1]
-            logical = sorted([len(item[1]) for item in change])[-1]
-            physical = sorted([len(item[2]) for item in change])[-1]
-            resource = sorted([len(item[3]) for item in change])[-1]
-            replacement = sorted([len(item[4]) for item in change])[-1]
-
-            click.echo('{0: <{col1}}  {1:<{col2}}  {2:<{col3}}  {3:<{col4}}  {4:<{col5}}'.format(*change,
-                                                                                                 col1=action,
-                                                                                                 col2=logical,
-                                                                                                 col3=physical,
-                                                                                                 col4=resource,
-                                                                                                 col5=replacement))
-            click.echo("\t")
-
+        display_changes(changes=change_set_changes, name=change_set_name, change_set=True)
         # Acts as a filter on past resource failures
         past_failures = [stack_event for stack_event in
                          cfn_client.describe_stack_events(StackName=stack_name)['StackEvents'] if
