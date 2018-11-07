f9a97332d358a24a69bfb2b7c9b9fea20c4f745f
Nicholas Christian <nd.christian@gmail.com>
2018-11-07 14:30:49 -0800

    Rework delete

--- lowearthorbit/delete.py
+++ lowearthorbit/delete.py
@@ -14,22 +14,16 @@ def delete_stacks(**kwargs):
 
     cfn_client = session.client('cloudformation')
 
-    identified_stacks = []
-    for stack in cfn_client.describe_stacks()['Stacks']:
-        try:
-            if "{}-".format(job_identifier) in stack['StackName']:
-                identified_stacks.append(stack['StackName'])
-        except IndexError:
-            # Non-Leo stacks
-            pass
+    stack_names = sorted([stack['StackName'] for stack in cfn_client.describe_stacks()['Stacks'] if
+                          "{}-".format(job_identifier) in stack['StackName']])
 
-    log.debug("Stacks to be deleted: %s" % identified_stacks)
-    for identified_stack in identified_stacks:
-        cfn_client.delete_stack(StackName=identified_stack)
+    click.confirm("Do you want to delete these stacks? : %s" % stack_names)
+    for stack_name in reversed(stack_names):
+        cfn_client.delete_stack(StackName=stack_name)
         try:
-            cfn_client.get_waiter('stack_delete_complete').wait(StackName=identified_stack)
-            click.echo("Deleted {}.".format(identified_stack))
+            cfn_client.get_waiter('stack_delete_complete').wait(StackName=stack_name)
+            click.echo("Deleted {}.".format(stack_name))
         except botocore.exceptions.WaiterError as waiter_error:
-            click.echo("{} failed to delete. {}".format(identified_stack, waiter_error))
+            click.echo("{} failed to delete. {}".format(stack_name, waiter_error))
             click.echo("Stopped stack deletion.")
             break
