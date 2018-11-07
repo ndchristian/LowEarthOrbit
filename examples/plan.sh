0bda75b9da0f1cdde36614c9a9bb15e51a71e27e
Nicholas Christian <nd.christian@gmail.com>
2018-11-07 15:15:29 -0800

    Parameters must be in quotes

--- examples/plan.sh
+++ examples/plan.sh
@@ -5,7 +5,9 @@ source environ.sh
 #echo $PROFILE
 #echo $BUCKET
 #echo $PREFIX
+#echo $JOBIDENTIFIER
+#echo $PARAMETERS
 
-leo plan --bucket $BUCKET --prefix $PREFIX --job-identifier $JOBIDENTIFIER  --parameters $PARAMETERS
+leo plan --bucket $BUCKET --prefix $PREFIX --job-identifier $JOBIDENTIFIER  --parameters "$PARAMETERS"
 
 exit
