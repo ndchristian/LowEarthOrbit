c2ce262a22a8b077959a705388f19e399080d399
Nicholas Christian <nd.christian@gmail.com>
2018-11-07 16:22:28 -0800

    Bump version to 0.5dev

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='LowEarthOrbit',
-    version='0.4dev',
+    version='0.5dev',
     author='Nicholas Christian',
     author_email='ndchristian@gmail.com',
     packages=setuptools.find_packages(),
